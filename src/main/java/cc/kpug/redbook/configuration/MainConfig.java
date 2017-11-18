9f8d77e85cc6be8e6def224505929b18454e3ec9
kLawrence <ultimate1352@gmail.com>
2017-11-18 16:22:36 +0900

    add h2 database

---
+++ src/main/java/cc/kpug/redbook/configuration/MainConfig.java
@@ -0,0 +1,12 @@
+package cc.kpug.redbook.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: Lawrence
+ * @since: 2017. 11. 18.
+ * @note:
+ */
+@Configuration
+public class MainConfig {
+}
